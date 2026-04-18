9ac8515daf5ab2cada5055444c89135d2b35631e
SoxCloud <sogcinwan@gmail.com>
2026-04-18 17:55:29 +0200

    ALL

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -8,7 +9,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, (process as any).cwd(), '');
 
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     define: {
       // Securely pass the environment variable to the client
       'process.env.API_KEY': JSON.stringify(env.API_KEY)
